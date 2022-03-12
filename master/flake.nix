93d855aba132d8263f5842aa3c16a1f4bb791c6d
hugosenari <hugosenari@gmail.com>
2022-03-12 03:32:27 -0300

    chore: re index testify

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''File-based unit testing system'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-testify-master.flake = false;
+  inputs.src-testify-master.owner = "sealmove";
+  inputs.src-testify-master.ref   = "refs/heads/master";
+  inputs.src-testify-master.repo  = "testify";
+  inputs.src-testify-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-testify-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-testify-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
