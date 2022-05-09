ba5400f60cc8c29e60f439400e5096f7a4067c7f
hugosenari <hugosenari@gmail.com>
2022-05-09 20:44:43 -0300

    chore: re index testify

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."testify-master".dir   = "master";
-  inputs."testify-master".owner = "nim-nix-pkgs";
-  inputs."testify-master".ref   = "master";
-  inputs."testify-master".repo  = "testify";
-  inputs."testify-master".type  = "github";
-  inputs."testify-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."testify-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
